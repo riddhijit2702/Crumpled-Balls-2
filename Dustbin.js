f36e23b5515c01a99e6eb615a0926f70614f0f4c
riddhijit2702 <74096278+riddhijit2702@users.noreply.github.com>
2020-11-25 18:36:55 +0530

    Update Dustbin.js

--- Dustbin.js
+++ Dustbin.js
@@ -12,10 +12,10 @@ class Dustbin{
     World.add(world,this.body)
     this.image=loadImage("dustbingreen.png")
     
-    this.rightbody=Bodies.rectangle(x+width/2,y-width/2,height,width,options)
-    World.add(world,this.rightbody)
+ this.rightbody=Bodies.rectangle(x+width/2,y-width/2,height,width,options)
+   World.add(world,this.rightbody)
 
-    this.leftbody=Bodies.rectangle(x-width/2,y-width/2,height,width,options)
+   this.leftbody=Bodies.rectangle(x-width/2,y-width/2,height,width,options)
     World.add(world,this.leftbody)
 
 /*this.x=x;
@@ -26,32 +26,33 @@ this.angle=angle*/
 display(){
 
 var pos =  this.body.position
-var pos2 =  this.rightbody.position
-var pos3 =  this.leftbody.position
+//var pos2 =  this.rightbody.position
+//var pos3 =  this.leftbody.position
 //var angle = this.body.angle
 
 push()
 translate(pos.x,pos.y)
 strokeWeight(2)
 fill("white")
-rect(0,0,this.width,this.height)
-pop();
-
-push()
-translate(pos2.x,pos2.y)
-strokeWeight(2)
-fill("white")
-rect(0,0,this.height,this.width)
-pop();
-
-push()
-translate(pos3.x,pos3.y)
-strokeWeight(2)
-fill("white")
-rect(0,0,this.height,this.width)
 imageMode(CENTER)
-image(this.image,100,20,250,250);
+image(this.image,0,-width/10);
+//rect(0,0,this.width,this.height)
 pop();
+
+//push()
+//translate(pos2.x,pos2.y)
+//strokeWeight(2)
+//fill("white")
+//rect(0,0,this.height,this.width)
+//pop();
+
+//push()
+//translate(pos3.x,pos3.y)
+//strokeWeight(2)
+//fill("white")
+//rect(0,0,this.height,this.width)
+
+//pop();
 }
 
 }
